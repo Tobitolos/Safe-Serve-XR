5e6a4c4832e24fcabd97f1642b5217b30b7db68b
Tobitolos <olatobiloba58@gmail.com>
2026-03-16 00:25:30 -0400

    Added restaurant floor mesh

--- js/restaurant.js
+++ js/restaurant.js
@@ -2,40 +2,65 @@ import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.158.0/build/three.m
 import { VRButton } from "https://cdn.jsdelivr.net/npm/three@0.158.0/examples/jsm/webxr/VRButton.js";
 
 let scene, camera, renderer;
+let mop;
+let spill;
+
+const feedback = document.getElementById("feedback");
 
 init();
 animate();
 
 function init() {
 
-scene = new THREE.Scene();
-scene.background = new THREE.Color(0xbfd1e5);
+    /*  SCENE SETUP  */
 
-camera = new THREE.PerspectiveCamera(
-75,
-window.innerWidth / window.innerHeight,
-0.1,
-1000
-);
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xbfd1e5);
 
-camera.position.set(0,1.6,4);
+    camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+    );
 
-renderer = new THREE.WebGLRenderer({antialias:true});
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.xr.enabled = true;
+    camera.position.set(0, 1.6, 4);
 
-document.body.appendChild(renderer.domElement);
-document.body.appendChild(VRButton.createButton(renderer));
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.xr.enabled = true;
 
-const light = new THREE.HemisphereLight(0xffffff,0x444444);
-scene.add(light);
+    document.body.appendChild(renderer.domElement);
+    document.body.appendChild(VRButton.createButton(renderer));
+
+    /* LIGHT */
+
+    const light = new THREE.HemisphereLight(0xffffff, 0x444444);
+    scene.add(light);
+
+
+
+    /* FLOOR */
+
+    const floorGeometry = new THREE.PlaneGeometry(10, 10);
+    const floorMaterial = new THREE.MeshStandardMaterial({ color: 0x999999 });
+
+    const floor = new THREE.Mesh(floorGeometry, floorMaterial);
+    floor.rotation.x = -Math.PI / 2;
+
+    scene.add(floor);
+
+
+/* ANIMATION LOOP */
+
+function animate() {
+
+    renderer.setAnimationLoop(render);
 
 }
 
-function animate(){
-renderer.setAnimationLoop(render);
-}
+function render() {
 
-function render(){
-renderer.render(scene,camera);
+    renderer.render(scene, camera);
+
 }
